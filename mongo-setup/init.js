65d8a7e7c86ff61f131c73a2e852b5ce04b1e353
Erwin <erwinxyz1@gmail.com>
2024-06-10 18:54:42 -0400

    stable config for postgres and mongo

--- mongo-setup/init.js
+++ mongo-setup/init.js
@@ -1,4 +1,12 @@
+var username = "mongo"; // Replace <username> with your desired username
+var password = "mongo123"; // Replace <password> with your desired password
+
 db = db.getSiblingDB('testdb'); // Create or switch to 'testdb'
+db.createUser({
+    user: username,
+    pwd: password,
+    roles: [{ role: "readWrite", db: "testdb" }] // Assign the user readWrite role for the testdb database
+});
 db.createCollection('data_table'); // Create 'data_table'
 //db.data_table.insertMany([
 //    { name: "Document 1", value: 100 },
