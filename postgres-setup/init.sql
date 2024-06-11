bdfebf817b9826b08c6723e3f012c6959d90fc62
Erwin <erwinxyz1@gmail.com>
2024-06-11 15:34:05 -0400

    Fixed postgres config issue

--- postgres-setup/init.sql
+++ postgres-setup/init.sql
@@ -7,6 +7,10 @@ CREATE TABLE IF NOT EXISTS data_table (
     random_col3 TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create a replication slot and publication for Debezium
+SELECT * FROM pg_create_logical_replication_slot('debezium', 'pgoutput');
+CREATE PUBLICATION dbserver1 FOR TABLE data_table;
+
 INSERT INTO data_table (random_col1, random_col2) VALUES ('sample data 1', 123);
 INSERT INTO data_table (random_col1, random_col2) VALUES ('sample data 2', 456);
 INSERT INTO data_table (random_col1, random_col2) VALUES ('sample data 3', 412);
